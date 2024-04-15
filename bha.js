9e3bbdce373f5b801a7750413da28423465a31a6
Pankajsr4 <kumarpankaj9982@gmail.com>
2024-04-15 22:24:26 +0530

    Online Grocery Store Website

---
+++ bha.js
@@ -0,0 +1,19 @@
+<script>
+	let pi = 3.14159265358979323846;
+
+	// Function to calculate the area of circle
+	function findArea(r)
+    {
+		return (pi * r * r);
+	}
+
+	// Driver code
+	let r, Area;
+	r = 5;
+
+	// Function calling
+	Area = findArea(r);
+
+	// displaying the area
+	console.log("Area of Circle is: " + Area);
+</script>
